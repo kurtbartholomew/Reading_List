dd415ef44a70b67046b44121f1fcde68b5db6306
Your Name <kurt@kurtbartholomew.com>
2014-11-04 17:14:44 -0800

    added search scope

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,19 @@ class Book < ActiveRecord::Base
 
 	scope :finished, ->{ where.not(finished_on: nil) }
 	scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
+	scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
 
 	#def self.recent
 	#	where('finished_on > ?', 2.days.ago)
 	#end
+
+	# def self.search(keyword)
+	# 	if keyword.present?
+	# 		where(title: keyword)
+	# 	else
+	# 		all
+	# 	end
+	# end
 
 	def finished?
 		finished_on.present?
